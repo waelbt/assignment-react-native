4605947241252a44c30d99ada446d0a1259a9448
dos <57960945+waelbt@users.noreply.github.com>
2024-04-13 18:23:49 +0000

    fix(ngrok): Correct port forwarding to expose port 3000 for Expo data fetching

--- app/components/ProductList.jsx
+++ app/components/ProductList.jsx
@@ -1,3 +1,4 @@
+import React, { useContext } from "react";
 import {
 	FlatList,
 	StyleSheet,
@@ -5,40 +6,56 @@ import {
 	TouchableOpacity,
 	View,
 } from "react-native";
-import React, { useContext } from "react";
 import { useProduct } from "../hooks";
 import DataContext from "../context";
 
 const ProductList = () => {
 	const { products, error } = useProduct();
-
 	const { setData } = useContext(DataContext);
 
 	if (error) {
 		return (
-			<View>
-				<Text>error fetching products</Text>
+			<View style={styles.centered}>
+				<Text>Error fetching products.</Text>
 			</View>
 		);
 	}
+
 	return (
-		<View>
-			<FlatList
-				data={products}
-				renderItem={({ product }) => (
-					<TouchableOpacity key={product.id} onPress={() => setData(product)}>
-						<View />
-						<View>
-							<Text>{product.product_name}</Text>
-							<Text>{product.price} Dhs</Text>
-						</View>
-					</TouchableOpacity>
-				)}
-			/>
-		</View>
+		<FlatList
+			data={products}
+			keyExtractor={(item) => item.id.toString()} // Assuming 'id' is a number, it needs to be converted to a string
+			renderItem={({ item }) => (
+				<TouchableOpacity onPress={() => setData(item)}>
+					<View style={styles.itemContainer}>
+						<Text style={styles.name}>{item.product_name}</Text>
+						<Text style={styles.price}>{item.price} Dhs</Text>
+					</View>
+				</TouchableOpacity>
+			)}
+		/>
 	);
 };
 
 export default ProductList;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	centered: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	itemContainer: {
+		padding: 10,
+		borderBottomWidth: 1,
+		borderBottomColor: "#cccccc",
+	},
+	name: {
+		fontSize: 16,
+		fontWeight: "bold",
+	},
+	price: {
+		fontSize: 14,
+		color: "#666666",
+	},
+});
--- app/hooks/useProduct.js
+++ app/hooks/useProduct.js
@@ -2,26 +2,27 @@ import { useEffect, useState } from "react";
 
 const useProduct = () => {
 	const [products, setProducts] = useState([]);
-    const [error, setError] = useState(false);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-                setError(false);
-				const reponse = await fetch("http://localhost:3000/products");
+				setError(false);
+				const reponse = await fetch(
+					"https://28a6-51-140-66-210.ngrok-free.app/products"
+				);
 				const data = await reponse.json();
 				setProducts(data);
 			} catch (error) {
-				console.log(error);
-                setError(true);
-            }
+				console.log(error.message);
+				setError(true);
+			}
 		};
 
 		fetchData();
 	}, []);
 
-
-    return {products, error}
+	return { products, error };
 };
 
 export default useProduct;
