d2832ae21f2e8e4e50ea21f027543b338951dea9
dos <57960945+waelbt@users.noreply.github.com>
2024-04-13 17:25:24 +0000

    intial setup

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { SearchBar, ProductList, ProductItem } from "./app/components";
+import { useEffect } from "react";
+
+export default function App() {
+
+	return <View style={styles.container}>
+
+  </View>;
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
---
+++ app/hooks/useProduct.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+
+const useProduct = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {}, []);
+};
+
+export default useProduct;
